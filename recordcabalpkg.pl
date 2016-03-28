895a0bca07853a8ca2e222db8bf0fc8072a4804d
gold-king <goldenking_x@yahoo.com>
2016-03-28 14:07:07 +0800

    This file is to record all installed cabal package.

---
+++ recordcabalpkg.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use File::Basename qw /basename/;
+
+my $cabal_path  = "/home/ucombinator/.cabal/lib/x86_64-linux-ghc-7.10.3/";
+my $record_file = "/home/ucombinator/cblpkg";
+
+my @libs = <"$cabal_path/*">;
+open(FD, '+>', $record_file)
+    or die "Open ERROR: $!E";
+
+print FD "--------------------------------------------------\n";
+
+for my $lib (@libs) {
+    if (-d $lib) {
+	print FD basename($lib). "\n";
+	
+    }
+}
+
+print FD "--------------------------------------------------\n\n\n";
+
+close(FD) or die "Close ERROR: $!";
