21471076f221a58f11dd9f3b82b028093428c88c
gold-king <goldenking_x@yahoo.com>
2016-03-31 20:18:29 +0800

    about sounde mute and unmute shell script.

---
+++ sound.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+amixer sset Master unmute
+
+#alsamixer
+#speaker-set
+#amixer
+#aplay -l
+#Path /proc/asound
+#Path /etc/asound/  file: asound.conf
+#URL https://wiki.archlinux.org/index.php/Alsa#Restore_ALSA_Mixer_settings_at_startup
