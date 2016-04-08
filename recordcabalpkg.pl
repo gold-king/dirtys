c233b34b7af4f31ba88d5b74a9987b289722b552
gold-king <goldenking_x@yahoo.com>
2016-04-09 00:29:50 +0800

    change direcotry

--- recordcabalpkg.pl
+++ recordcabalpkg.pl
@@ -3,8 +3,8 @@ use strict;
 use warnings;
 use File::Basename qw /basename/;
 
-my $cabal_path  = "/home/ucombinator/.cabal/lib/x86_64-linux-ghc-7.10.3/";
-my $record_file = "/home/ucombinator/cblpkg";
+my $cabal_path  = "/home/gk/.cabal/lib/x86_64-linux-ghc-7.10.3/";
+my $record_file = "/home/gk/cblpkg";
 
 my @libs = <"$cabal_path/*">;
 open(FD, '+>', $record_file)
