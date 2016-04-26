2b154999ddc15c529d4f4512a51a04dc18bbf0f1
gold-king <goldenking_x@yahoo.com>
2016-04-26 13:31:51 +0800

    change file write format for more readable

--- recordcabalpkg.pl
+++ recordcabalpkg.pl
@@ -3,20 +3,19 @@ use strict;
 use warnings;
 use File::Basename qw /basename/;
 
-my $cabal_path  = "/home/gk/.cabal/lib/x86_64-linux-ghc-7.10.3/";
-my $record_file = "/home/gk/cblpkg";
+my $cabal_path  = "/home/ucombinator/.cabal/lib/x86_64-linux-ghc-7.10.3/";
+my $record_file = "/home/ucombinator/cblpkg";
 
 my @libs = <"$cabal_path/*">;
 open(FD, '+>', $record_file)
     or die "Open ERROR: $!E";
 
-print FD "--------------------------------------------------\n";
+print FD "Installed Packages:\n";
 
 for my $lib (@libs) {
     if (-d $lib) {
-	print FD basename($lib). "\n";
+	print FD "  ". basename($lib). "\n";	
     }
 }
 
-print FD "--------------------------------------------------\n";
 close(FD) or die "Close ERROR: $!";
