e9b05f1a47679e124bc5911cac7ac60929a27c24
gold-king <goldenking_x@yahoo.com>
2016-05-09 21:48:11 +0800

    change format & added datetime

--- recordcabalpkg.pl
+++ recordcabalpkg.pl
@@ -2,14 +2,17 @@
 use strict;
 use warnings;
 use File::Basename qw /basename/;
+use POSIX qw /strftime/;
 
-my $cabal_path  = "/home/ucombinator/.cabal/lib/x86_64-linux-ghc-7.10.3/";
-my $record_file = "/home/ucombinator/cblpkg";
+my $cabal_path  = "/home/gk/.cabal/lib/x86_64-linux-ghc-7.10.3/";
+my $record_file = "/home/gk/cblpkg";
+my $date_time   = strftime "%F %I:%M:%S", localtime;
 
 my @libs = <"$cabal_path/*">;
-open(FD, '+>', $record_file)
+open(FD, '>>', $record_file)
     or die "Open ERROR: $!E";
 
+print FD $date_time, "\n";
 print FD "Installed Packages:\n";
 
 for my $lib (@libs) {
@@ -17,5 +20,7 @@ for my $lib (@libs) {
 	print FD "  ". basename($lib). "\n";	
     }
 }
+
+print FD "-----------------------------------------------------------------\n\n";
 
 close(FD) or die "Close ERROR: $!";
