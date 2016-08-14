e7f8f61d9511db2ccfe570faed54319629915fe8
gold-king <goldenking_x@yahoo.com>
2016-08-14 11:33:28 +0800

    print current date & time

---
+++ showtime.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 20
+
+int main(void)
+{
+	static time_t t;
+	static struct tm *ltp;
+	static char buf[50];
+
+	t = time(NULL);
+	ltp = localtime(&t);
+	if (ltp == NULL) {
+		fputs("Localtime Error!\n", stdout);
+	}
+
+	if (strftime(buf, SIZE, "%F %I:%M:%S", ltp) == 0) {
+	  fprintf(stderr, "STRFTIME function error.\n");
+	  exit(1);
+	}
+
+	fprintf(stdout, "Current time is: %s\n", buf);
+	return 0;
+}
